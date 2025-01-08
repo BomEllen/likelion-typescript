e4478de306d0f776e467ae9228aa934ba757ac85
BomEllen <jjjj200100@gmail.com>
2025-01-08 17:27:40 +0900

    [1/8] 학습 완료

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,60 @@
 // --------------------------------------------------------------------------
 // TypeScript + Express.js를 활용해 간단한 API 서버 구성
 // --------------------------------------------------------------------------
+
+import "dotenv/config";
+import type { Express } from "express";
+import express from "express";
+import { resolve } from "node:path";
+
+import usersRouter from "./routes/users";
+
+/* CONFIG. ------------------------------------------------------------------ */
+
+// for Windows Users
+const HOSTNAMES = "localhost";
+// const HOSTNAMES = process.env.HOSTNAMES ?? 'localhost';
+const PORT = Number(process.env.PORT) ?? 4000;
+const MESSAGE = `웹 서버 구동 : http://${HOSTNAMES}:${PORT}`;
+
+/* Application -------------------------------------------------------------- */
+
+const app: Express = express();
+
+/* Middleware --------------------------------------------------------------- */
+
+// app.use(greetingMessage);
+app.use(express.static(resolve(__dirname, "../public")));
+app.use(express.json());
+
+/* Routing ------------------------------------------------------------------ */
 //
 // 라우팅은 특정 엔드포인트에 대한 클라이언트 요청에 애플리케이션이 어떻게 응답할지 결정하는 것을 말하며,
 // 이는 URI(또는 경로)와 특정 HTTP 요청 메서드(GET, POST, PUT, PATCH, DELETE 등)입니다.
 // 각 경로에는 하나 이상의 핸들러 함수가 있을 수 있으며, 이 함수는 경로가 일치할 때 실행됩니다.
 //
-// 이미지, 스타일, 스크립트 파일과 같은 정적 파일을 제공하려면 기본 제공되는 미들웨어 함수를 사용합니다.
-// 여러 정적 에셋 디렉토리를 사용하려면 express.static 미들웨어 함수를 여러 번 호출합니다.
-//
 // --------------------------------------------------------------------------
-import "dotenv/config";
-import express from "express";
-import type { Express } from "express";
-import entryHandler from "./handlers/entry";
-import greetingMessage from "./middlewares/greetingMessage";
 
-const app: Express = express();
+// GET
+// app.get('/', entryHandler);
 
-// for Windows Users
-const HOSTNAME = "localhost";
-// const HOSTNAME = process.env.HOSTNAME ?? 'localhost';
-const PORT = Number(process.env.PORT) ?? 4000;
-const MESSAGE = `웹 서버 구동 : http://${HOSTNAME}:${PORT}`;
+// POST
+// app.post('/', (request, response) => {
+//   // 클라이언트 요청 URL
+//   console.log(request.url);
 
-/* Middleware --------------------------------------------------------------- */
-app.use(greetingMessage);
-app.use(express.static('public'));
+//   // 서버 -> 클라이언트 응답
+//   response.status(201 /* Created */).send({
+//     message: 'POST 요청이 홈페이지로부터 주어졌습니다.',
+//   });
+// });
 
-/* Routing ------------------------------------------------------------------ */
+/* API ---------------------------------------------------------------------- */
 
-app.get("/", entryHandler);
+app.use("/api/users", usersRouter);
 
-app.listen(PORT, HOSTNAME, () => {
+/* Listening ---------------------------------------------------------------- */
+
+app.listen(PORT, HOSTNAMES, () => {
   console.log(MESSAGE);
 });
--- src/handlers/entry.ts
+++ src/handlers/entry.ts
@@ -1,15 +1,19 @@
-import { resolve } from "node:path";
-import { readFile } from "node:fs/promises";
-import type { Response, Request, NextFunction } from "express";
+import { resolve } from 'node:path';
+import { readFile } from 'node:fs/promises';
+import type { Response, Request, NextFunction } from 'express';
 
-async function entryHandler(request: Request, response: Response, nextFunction: NextFunction) {
+async function entryHandler(
+  request: Request,
+  response: Response,
+  nextFunction: NextFunction
+) {
   // 서버 애플리케이션의 로컬 저장소 위치의 파일 비동기 방식으로 읽기
   // fsPromises.readFile(path: string, options?)
   // __dirname === new URL('./', import.meta.url)
   try {
-    const entryFilePath = resolve(__dirname, "../index.html");
+    const entryFilePath = resolve(__dirname, '../index.html');
     const entryFileCode = await readFile(entryFilePath, {
-      encoding: "utf-8",
+      encoding: 'utf-8',
     });
 
     // 서버 -> 클라이언트 응답(response)
